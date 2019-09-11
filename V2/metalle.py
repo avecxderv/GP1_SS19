5be579da460577d48900fb5c2c8e2d9339c86892
avecxderv <simon@schwarz-ibs.de>
2019-09-11 13:13:55 +0200

    Versuchsdurchfuehrung und Rohdaten plts

--- V2/metalle.py
+++ V2/metalle.py
@@ -20,13 +20,13 @@ l9 = 129.9
 l11 = 130.1
 
 d5 = 11.991e-1
-ed5 =  0.0019e-1
+ed5 =  0.0801e-1
 d6 = 11.978e-1
-ed6 = 5.6218e-4
+ed6 = 0.0042e-1
 d9 = 11.944e-1
-ed9 = 3.0358e-5
+ed9 = 0.031e-1
 d11 = 11.989e-1
-ed11 = 6.4265e-5
+ed11 = 0.0451e-1
 
 m5 = 1295
 m6 = 1237.2
@@ -37,21 +37,70 @@ el = 0.69e-3
 em = 0.1e-3/np.sqrt(12)
 
 rho_cu = 4*m5/(np.pi*d5**2*l5)
+erho_cu = rho_cu*np.sqrt( 0.1**2/(m5**2) + 0.0069**2/(l5**2) + 4*(ed5**2)/(d5**2))
 rho_messing = 4*m6/(np.pi*d6**2*l6)
+erho_messing = rho_messing*np.sqrt( 0.1**2/(m6**2) + 0.0069**2/(l6**2) + 4*ed6**2/(d6**2))
 rho_al = 4*m9/(np.pi*d9**2*l9)
+erho_al = rho_al*np.sqrt( 0.1**2/(m9**2) + 0.0069**2/(l9**2) + 4*(ed9**2)/(d9**2))
 rho_stahl = 4*m11/(np.pi*d11**2*l11)
+erho_stahl = rho_stahl*np.sqrt( 0.1**2/(m11**2) + 0.0069**2/(l11**2) + 4*ed11**2/(d11**2))
 
 '''
-data = cassy.CassyDaten('daten/Nr11_4.lab')
+#Plot Rohdaten
+data = cassy.CassyDaten('daten/Nr5_1.lab')
+timeVal = data.messung(1).datenreihe('t').werte
+voltage = data.messung(1).datenreihe('U_A1').werte
+f, (ax1,ax2) = plt.subplots(2,1)
+ax1.plot(timeVal,voltage)
+ax1.set_ylabel("Schalldruck")
+ax2.set_ylabel("Schalldruck")
+ax2.plot(timeVal[0:250], voltage[0:250])
+ax2.set_xlabel("$t$ / $s$")
+ax1.set_xlim(0,1.6)
+ax2.set_xlim(0,0.025)
+plt.rcParams["figure.figsize"] = (12,6)
+plt.rcParams['axes.titlesize'] = 'large'
+plt.rcParams['axes.labelsize'] = 'large'
+plt.savefig('plots/rohdaten.pdf')
+plt.show()
+plt.close()
+'''
+
+'''
+#Beispiel Sepektrum
+data = cassy.CassyDaten('daten/Nr5_1.lab')
 timeVal = data.messung(1).datenreihe('t').werte
 voltage = data.messung(1).datenreihe('U_A1').werte
 fourier = analyse.fourier_fft(timeVal, voltage)
 freq = fourier[0]
 amp = fourier[1]
-peak = analyse.peakfinder_schwerpunkt(freq[1200:1600],amp[1200:1600])
-
-plt.plot(freq[0:8000],amp[0:8000])
+plt.plot(freq[0:8000],amp[0:8000], color='orange')
+plt.xlim(0,5000)
+plt.xlabel('$f$ / Hz')
+plt.ylabel('Amplitude')
+plt.grid()
+plt.rcParams["figure.figsize"] = (12,6)
+plt.rcParams['axes.titlesize'] = 'large'
+plt.rcParams['axes.labelsize'] = 'large'
+plt.savefig('plots/spektrum1.pdf')
 '''
+
+
+data = cassy.CassyDaten('daten/Nr5_1.lab')
+timeVal = data.messung(1).datenreihe('t').werte
+voltage = data.messung(1).datenreihe('U_A1').werte
+fourier = analyse.fourier_fft(timeVal, voltage)
+freq = fourier[0]
+amp = fourier[1]
+plt.scatter(freq,amp,color='orange')
+maximumIndex = amp.argmax();
+plt.xlim(freq[max(0, maximumIndex-10)], freq[min(maximumIndex+10, len(freq))])
+peak = analyse.peakfinder_schwerpunkt(freq, amp)
+plt.axvline(peak, linestyle= "--",color='black')
+#plt.plot(freq[0:8000],amp[0:8000])
+
+
+
 
 f0_5 = [1458.77, 1458.8, 1458.82, 1458.83, 1458.83]
 f_5 = np.sum(f0_5)/5
