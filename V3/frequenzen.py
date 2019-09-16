a9d878b9e77e89906d5a96149ff8b3a387a7bb67
avecxderv <simon@schwarz-ibs.de>
2019-09-16 18:08:54 +0200

    Mhhh leckere Numerik

--- V3/frequenzen.py
+++ V3/frequenzen.py
@@ -10,28 +10,54 @@ from praktikum import analyse
 from praktikum import cassy
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
 
 m = 16e-3
 em = 0.1/np.sqrt(12)
 
+def func(x, a, b, c, d,e):
+    return a*np.exp(-b*x)*np.sin(c*x+e) + d
+
+
+def korrektur(time,pres):
+    popt, pcov = curve_fit(func, time,pres)
+    return popt[1]
+
 def fft_analysis(groesse,hoehe,nummer):
+    #Daten mit Korrektur
     dataname = 'daten/' + groesse + '_' + hoehe + '_' + nummer + '.lab'
     data = cassy.CassyDaten(dataname)
     time = data.messung(1).datenreihe('t').werte
-    volt = data.messung(1).datenreihe('p_A1').werte
-    fourier = analyse.fourier_fft(time,volt)
+    pres = data.messung(1).datenreihe('p_A1').werte
+    fourier = analyse.fourier_fft(time,pres)
+    freq = fourier[0]
+    amp = fourier[1]
+    peak = analyse.peakfinder_schwerpunkt(freq[10:100],amp[10:100])
+    kor = korrektur(time,pres)
+    return np.sqrt(peak**2+kor**2)
+
+def fft_analysis_ohnekor(groesse,hoehe,nummer):
+    #Daten ohne Korrektur
+    dataname = 'daten/' + groesse + '_' + hoehe + '_' + nummer + '.lab'
+    data = cassy.CassyDaten(dataname)
+    time = data.messung(1).datenreihe('t').werte
+    pres = data.messung(1).datenreihe('p_A1').werte
+    fourier = analyse.fourier_fft(time,pres)
     freq = fourier[0]
     amp = fourier[1]
     peak = analyse.peakfinder_schwerpunkt(freq[10:100],amp[10:100])
     return peak
 
 def mittel(groesse,hoehe):
+    #Standardmaessig mit Korrektur
     #groesse ist aus {'kleine', 'grosse', 'mittel'}
     f = [fft_analysis(groesse,hoehe,'1'), fft_analysis(groesse,hoehe,'2'),
          fft_analysis(groesse,hoehe,'3'), fft_analysis(groesse,hoehe,'4'),
          fft_analysis(groesse,hoehe,'5')]
     ef = np.full((5,),0.5)
     return analyse.gewichtetes_mittel(f,ef)
+
 
 f_kl_15 = mittel('kleine','15')
 f_kl_20 = mittel('kleine','20')
