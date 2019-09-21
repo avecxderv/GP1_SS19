4e9ad6f9483014bb4f84278d90eba57da13d1aac
lormaris <lorenz.ising@rwth-aachen.de>
2019-09-21 14:04:38 +0200

    rohdaten.py

---
+++ V4/rohdaten.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 21 13:39:02 2019
+
+@author: Marius
+"""
+
+from praktikum import analyse
+from praktikum import cassy
+import numpy as np
+import matplotlib.pyplot as plt
+
+data_s = cassy.CassyDaten('daten/schwingung_10Ohm_1.lab')
+data_k = cassy.CassyDaten('daten/schwingung_1000Ohm_1.lab')
+data_a = cassy.CassyDaten('daten/aperiodisch_3_186.lab')
+
+time = data_s.messung(1).datenreihe('t').werte
+# Zeit in ms
+time = time*1000
+volt_s = data_s.messung(1).datenreihe('U_B1').werte
+volt_k = data_k.messung(1).datenreihe('U_B1').werte
+volt_a = data_a.messung(1).datenreihe('U_B1').werte
+
+plt.figure(figsize=(12,6))
+
+plt.plot(time, volt_s, color='blue', label='Schwingung')
+plt.plot(time, volt_k, color='green', label='Kriechfall')
+plt.plot(time, volt_a, color='red', label='Aperiodischer Grenzfall')
+plt.xlim(0,5)
+plt.grid()
+plt.xlabel('$t$ / ms')
+plt.ylabel('$U$ / V')
+plt.legend()
+
+plt.savefig('plots/rohdaten_ska.pdf', format='pdf', dpi=1200)
+
+plt.show()
